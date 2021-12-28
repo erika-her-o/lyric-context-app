96c75533f3112d43af0cc21da52dc2ef5f267818
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-12-28 16:43:04 -0300

    add SongsContext

--- src/contexts/SongsContext.js
+++ src/contexts/SongsContext.js
@@ -1,9 +1,46 @@
+import { ControlPointSharp } from '@material-ui/icons';
 import React, { createContext, useState, useEffect } from 'react';
 import { chartTracksGet, trackSearch } from "./../constants";
 
 const SongsContextProvider = () => {
     const [ doneFetch, setDoneFetch ] = useState();
-    
+    const [ currentQTrack, setCurrentQTrack ] = useState("");
+    const [ text, setText ] = useState("Top Songs");
+    const [ tracks, setTracks ] = useState([]);
+
+    //didMount, didUpDate, willUnmount
+    useEffect(() => {
+        getTopTracks();
+    }, []);
+
+    const getTopTracks = () => {
+        fetch(chartTracksGet())
+        .then(res => res.json())
+        .then(data => {
+            setDoneFetch(true)
+            setTracks(data.message.body.tracks_list);
+        })
+        .catch(err => console.log(err));
+    };
+
+    const getTrack = q_track => {
+        fetch(trackSearch(q_track))
+        .then(res => res.json())
+        .then(data => {
+            const { track_list } = data.message.body;
+            setDoneFetch(true);
+            setText(track_list.length? "Results" : "No Results");
+            setTracks(track_list);
+        })
+        .catch(err => ControlPointSharp.log(err));
+    };
+
+    const validateQTrack = (e, q_track = document.querySelector("#q_track").value.toLowerCase().trim())=>{
+        if() {
+            
+        }
+
+    }
 }
 
 export default SongsContextProvider;
