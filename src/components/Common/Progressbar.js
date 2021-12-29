aaf7ec1e58e63fef213f5506822561e18de22cf4
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-12-28 22:03:30 -0300

    changes in Message.js , Progressabar, SearchTracks.js and Tracks.js

---
+++ src/components/Common/Progressbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Progressbar = () => {
+    return (
+        <div>
+            <p>progressBar</p>
+        </div>
+    )
+}
+
+export default Progressbar
---
+++ src/components/Songs/Tracks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tracks = () => {
+    return (
+        <div>
+            <p>ACA ES TRACKS</p>
+        </div>
+    )
+}
+
+export default Tracks;
